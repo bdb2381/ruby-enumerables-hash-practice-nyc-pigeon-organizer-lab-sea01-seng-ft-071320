be9206684820ab1cccc4b097cf2b65530bbbf76f
Brad Beggs <brad.beggs@gmail.com>
2020-07-01 23:44:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,6 +5,9 @@ def nyc_pigeon_organizer(data)
   data.map do | key, value, index|
     print data[key][value][index]
   end
+
+
+end 
 =begin
 "Theo" = pigeon_data[:color][:purple][0], [:gender][:male][1] =>theo
 find all the names in the hash of arrays

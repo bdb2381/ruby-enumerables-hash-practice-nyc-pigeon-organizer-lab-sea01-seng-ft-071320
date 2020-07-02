14c836e2010ace6c61afc548b703da6a71cbe80a
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 00:12:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,11 +3,11 @@ def nyc_pigeon_organizer(data)
   # write your code here!
 
 
-#  data.each do | key, value, index|
-  # data[:key][:key][index]
-   binding.pry
+#  data.each do | key, value|
+  # data[:key][value]
+#   binding.pry
 #  end
-
+print data
 
 end
 =begin

aa8b7afd11a953b558e591ecd796300f677a5616
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 01:45:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,9 +5,10 @@ def nyc_pigeon_organizer(data)
     value.each do |array_key, bird_names|  #loop down to the value level of arrays of names
       bird_names.each do |name|
         if !final[name]
-          final[name] = {:color => [], :gender => [], :lives => []}   #create a hash with the keys and their blank values in an array
-        #  binding.pry
+          final[name] = {}#{:color => [], :gender => [], :lives => []}   #create a hash with the keys and their blank values in an array
         end #end if statement
+        if !final[key] = []
+binding.pry
       end #end loop of names
     end #end loop of values
   end #end loop of each_with_object

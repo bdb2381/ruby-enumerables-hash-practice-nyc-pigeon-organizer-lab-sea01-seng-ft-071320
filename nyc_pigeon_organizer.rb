48c0629016000030ac39ff7dc9dc38e6d6934d0d
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 01:11:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do | (key, value), final|
     value.each do |array_key, bird_names|
       bird_names.each do |name|
-        print name
+        print name.unique
       end
     end
   end

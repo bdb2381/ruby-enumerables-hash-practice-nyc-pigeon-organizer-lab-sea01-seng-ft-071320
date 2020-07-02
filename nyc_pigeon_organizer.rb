c9d3e1fda240fdb93870f233ffed4cd2f08cdb1a
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 01:09:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
-  #names = {}
+  names = {}
   names = data.each_with_object({}) do | (key, value), final|
     value.each do |array_key, bird_names|
       bird_names.each do |name|

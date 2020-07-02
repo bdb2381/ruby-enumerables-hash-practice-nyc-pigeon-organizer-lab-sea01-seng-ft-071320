5bff46a824569dae3e5a7566051a92ed2d29f5b0
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 00:37:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
 
-
   data.each_with_object({}) do | (key, value), final|
-    final[k] = v
+    final #[k] = v
 binding.pry
     end
 #   binding.pry

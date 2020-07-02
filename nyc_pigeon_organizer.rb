5fea4f81709744347abe78e021e8c31abe7d2bf4
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 00:38:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
 
   data.each_with_object({}) do | (key, value), final|
-    final [k] = v
+    final 
     binding.pry
   end
 #print data[:color][:purple][0]

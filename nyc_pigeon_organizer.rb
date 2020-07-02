41e6341a7516720674afcea38e9412bc534d78e6
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 00:38:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,14 +3,15 @@ def nyc_pigeon_organizer(data)
   # write your code here!
 
   data.each_with_object({}) do | (key, value), final|
-    final #[k] = v
-binding.pry
-    end
-#   binding.pry
+    final [k] = v
+    binding.pry
   end
 #print data[:color][:purple][0]
 
 end
+
+
+
 =begin
 "Theo" = pigeon_data[:color][:purple][0], [:gender][:male][1] =>theo
 find all the names in the hash of arrays
